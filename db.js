f77b3a78c9dd24ea0f359e8f7c9aa4434ae2b6f8
lucassoares02 <lucas.soares02@hotmail.com>
2026-03-09 11:20:45 -0300

    remove ssl db

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const pool = new Pool({
   // ssl: false,
   connectionString: process.env.POSTGRESQL_EXTERNAL_URL,
-  ssl: { rejectUnauthorized: false }, // para Render
+  // ssl: { rejectUnauthorized: false }, // para Render
 });
 
 module.exports = pool;
